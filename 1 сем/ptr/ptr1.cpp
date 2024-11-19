ea582f69ba1a36fbd649ee0bb137f05f49b7e9ab
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-11-19 21:57:53 +0500

    Add files via upload

---
+++ 1 сем/ptr/ptr1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	int* aptr;
+	int* bptr;
+
+	cin >> a;
+	aptr = &a;
+	c = *aptr;
+
+	cin >> b;
+	bptr = &b;
+
+	*aptr = *bptr;
+	*bptr = c;
+
+	cout << *aptr << endl;
+	cout << *bptr << endl;
+	return 0;
+}
