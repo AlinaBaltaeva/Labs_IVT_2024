fb7b8b5ab1d02d166c8facabd9210c783efc219f
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-11-19 22:44:22 +0500

    Add files via upload

---
+++ 1 сем/2.2/2.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int N, c;
+    cin >> N;
+    int i = 1;
+    c = 1;
+    while (i <= N) {
+        c *= i;
+        i++;
+    }
+    cout << c << endl;
+    return 0;
+}
+
