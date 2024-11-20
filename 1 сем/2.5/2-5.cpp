22dfc181fefa0d7b9ff23eb49ad191b6399fc9e5
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-11-20 21:02:03 +0500

    Add files via upload

---
+++ 1 сем/2.5/2-5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 2 )
+    {
+        cout << "select a different value" << endl;
+    }
+    else
+    {
+        int p = 1;
+        int t = n-1;
+        for (int i = 0; i <= (n-1) ; i++)
+        {
+            for (int j = 0; j <t; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 0; j < p; j++)
+            {
+                cout << "*";
+            }
+            t--;
+            p++;
+            cout << endl;
+        }
+       
+    }
+    return 0;
+}
