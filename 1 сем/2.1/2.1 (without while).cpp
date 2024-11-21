85485d8754084f5a6eabf98b934b2063f705dc35
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-11-21 15:51:07 +0500

    Add files via upload

---
+++ 1 сем/2.1/2.1 (without while).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int sum;
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "enter another number" << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			sum += 1;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
