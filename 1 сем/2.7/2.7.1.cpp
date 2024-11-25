88a4e9d08768662932211843c9761cc47ca8d065
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-11-25 21:36:46 +0500

    Add files via upload

---
+++ 1 сем/2.7/2.7.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n, k;
+	cin >> n;
+	k = sqrt(n); //берем квадратный корень от n и передаем его в целое число k 
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
