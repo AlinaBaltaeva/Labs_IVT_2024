38c9af17db42fefc95a56368788e4503a0429e28
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-12-04 21:05:57 +0500

    Add files via upload

---
+++ 1 сем/2.11/2.11.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n, k;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		if (k != 0)
+		{
+			if (k > 0)
+			{
+				cout << "положительное число раньше отрицательного";
+				break;
+			}
+			else
+			{
+				cout << "отрицательное число раньше положительного"<< endl;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
