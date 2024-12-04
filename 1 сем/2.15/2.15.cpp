6dadfb324c09d63b05e1e1b087c470fc5f2f754e
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-12-04 21:09:16 +0500

    Add files via upload

---
+++ 1 сем/2.15/2.15.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, s, k;
+	bool flag = false;
+
+	cin >> n >> s;
+
+	while (n > 0 && !flag)
+	{
+		flag = true;
+		cout << "цифра найдена!" << endl;
+	}
+	if (!flag)
+	{
+		cout << "цифра найдена!" << endl;
+		return 0;
+	}
+}
