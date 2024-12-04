8e0cb903d76d9ceb1bafa826c3244417a5709a7a
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-12-04 21:05:13 +0500

    Add files via upload

---
+++ 1 сем/2.10/2.10 code.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n, max, k;
+	cin >> n;
+	cout << "введите первое число последовательности:" << endl;
+	cin >> max;
+	for (int i = 2; i <= n; i++)
+	{
+		cout << "введите следующее число: " << endl;
+		cin >> k;
+
+		if (k > max)
+		{
+			max = k;
+		}
+	}
+	cout <<"максимальный элемент :" << max << endl;
+	return 0;
+}
