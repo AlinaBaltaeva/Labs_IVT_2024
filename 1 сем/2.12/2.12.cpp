dd656408f475b4932b80dfe502d653b85612b501
AlinaBaltaeva <baltaeva06@inbox.ru>
2024-12-04 21:06:52 +0500

    Add files via upload

---
+++ 1 сем/2.12/2.12.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, k, max, min;
+	cout << "" << endl;
+	cin >> n;
+	cout << "" << endl;
+	cin >> k;
+	max = k;
+	min = k;
+	for (int i = 2; i <= n; i++)
+	{
+		cout << "" << endl;
+		cin >> k;
+		if (max < k)
+		{
+			max = k;
+		}
+		else if (min > k)
+		{
+			min = k;
+		}
+	}
+	cout <<"Сумма максимального и минисального элемнтов:"<<(max+min)<< endl;
+	return 0;
+}
