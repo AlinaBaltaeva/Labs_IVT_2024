32d3d8ee1330ca1aad1d3c26327847275057f765
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-05 18:45:21 +0500

    Add files via upload

---
+++ 2 сем/Sorting/пузырьковая сортировка.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int r, range_min, range_max, tmp;
+	int arr[10];
+	const int n = 10;
+	range_min = 2;
+	range_max = 280000;
+	for (int i = 0; i < 10; i++)
+	{
+		arr[i] = ((double)rand() / RAND_MAX) * (range_max -
+			range_min) + range_min;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
