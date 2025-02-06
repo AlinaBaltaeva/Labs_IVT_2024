14bcca1161e027b580f717291152380edfbc7c10
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-06 10:12:40 +0500

    Update пузырьковая сортировка.cpp

--- 2 сем/Bubble sorting/пузырьковая сортировка.cpp
+++ 2 сем/Bubble sorting/пузырьковая сортировка.cpp
@@ -6,45 +6,33 @@ int main()
 {
     int r, range_min, range_max, tmp;
 	srand(time(0));
-	const int n = 3;
-	int arr[n][n];
+	const int n = 10;
+	int arr[n];
 	range_min = 2;
 	range_max = 280000;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			arr[i][j] = ((double)rand() / RAND_MAX) * (range_max -range_min) + range_min;
-			cout << arr[i][j] << " ";
-		}
-		cout<<endl;
+		arr[i] = ((double)rand() / RAND_MAX) * (range_max -range_min) + range_min;
+			cout << arr[i] << " ";
 	}
 	cout << endl;
 
-	for (int i = 0; i < n ; i++)
+	for (int i = 0; i < n-1 ; i++)
 	{
-		for (int j = 0; j < n ; j++)
+		for (int j = 0; j < n-1-i ; j++)
 		{
-			for (int k = n-2; k>=0 ; k--)
-			{
-				if (arr[j][k] > arr[j][k+1])
+			if (arr[j] > arr[j+1])
 				{
-					tmp = arr[j][k];
-					arr[j][k] = arr[j][k+1];
-					arr[j][k+1] = tmp;
+					tmp = arr[j];
+					arr[j]= arr[j+1];
+					arr[j+1] = tmp;
 				}
-			}
-			
 		}
 	}
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
+		cout << arr[i] << " ";
 	}
 	cout << endl;
 	return 0;
