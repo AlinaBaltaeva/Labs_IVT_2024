b94ab683527fe7e71e453bfac92d4217848c8bfa
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-08 23:01:03 +0500

    Add files via upload

---
+++ 2 сем/Han_town/Han_town.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void han_town(int n, int start, int temp, int point)
+{
+	if (n > 0)
+	{
+		han_town(n - 1, start, temp, point);
+		cout << start << "=>" << point;
+		han_town(n - 1, temp, point, start);
+	}
+}
+int main()
+{
+	int n, start, point;
+	setlocale(LC_ALL, "rus");
+	cout << "ВВедите количество дисков" << endl;
+	cin >> n; 
+	han_town(n, 1, 3, 2);
+	return 0;
+}
