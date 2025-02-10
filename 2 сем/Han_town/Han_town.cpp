bb476c0cf25ae8421c0dea4a179001a483d734e1
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-10 09:52:01 +0500

    Add files via upload

---
+++ 2 сем/Han_town/Han_town.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+void towerOfHanoi(int n, char st, char fin, char tmp)
+{
+    setlocale(LC_ALL, "rus");
+    if (n == 1) {
+        
+        cout << "Первый диск движется из " << st
+            << " в стержень " << fin << endl;
+        return;
+    }
+    towerOfHanoi(n - 1, st, tmp, fin);
+
+    cout << "Движение диска " << n << " из стержня " << st
+        << " в стержень " << fin << endl;
+    towerOfHanoi(n - 1, tmp, fin, st);
+}
+
+int main()
+{
+    
+    int n = 3;
+    towerOfHanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
