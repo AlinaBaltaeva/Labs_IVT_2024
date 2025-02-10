c88c453b53043f7c12d0df1d963de1dc433e6c55
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-10 10:19:13 +0500

    Update Han_town.cpp

--- 2 сем/Han_town/Han_town.cpp
+++ 2 сем/Han_town/Han_town.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
-void towerOfHanoi(int n, char st, char fin, char tmp)
+void Han_town(int n, char st, char fin, char tmp)
 {
     setlocale(LC_ALL, "rus");
     if (n == 1) {
@@ -9,18 +9,18 @@ void towerOfHanoi(int n, char st, char fin, char tmp)
             << " в стержень " << fin << endl;
         return;
     }
-    towerOfHanoi(n - 1, st, tmp, fin);
+    Han_town(n - 1, st, tmp, fin);
 
     cout << "Движение диска " << n << " из стержня " << st
         << " в стержень " << fin << endl;
-    towerOfHanoi(n - 1, tmp, fin, st);
+    Han_town(n - 1, tmp, fin, st);
 }
 
 int main()
 {
     
     int n = 3;
-    towerOfHanoi(n, 'A', 'C', 'B');
+    Han_town(n, 'A', 'C', 'B');
 
     return 0;
 }
