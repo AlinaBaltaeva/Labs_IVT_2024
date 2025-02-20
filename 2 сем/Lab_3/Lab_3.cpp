fab187629e05a888a47e20a30492a9b3ea152294
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-21 00:46:47 +0500

    Add files via upload

---
+++ 2 сем/Lab_3/Lab_3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    system("chcp 1251");
+    system("cls");
+    string str;
+    getline(cin, str);
+    string str1 = ""; // для текущего слова
+    string str2 = ""; // для самого длинного слова
+    string str3 = ""; // для самого короткого слова
+    int k = 0; // длина текущего слова
+    int max = 0; // максимальная длина
+    int min = 100000; // минимальная длина
+
+    for (int i = 0; i <= str.length(); i++) // увеличил длину на 1, чтобы захватить последнее слово
+    {
+        if (str[i] != ' ')
+        {
+            k++;
+            str1 += str[i];
+        }
+        else
+        {
+            if (k > 0) // если слово закончилось
+            {
+                if (k < min)
+                {
+                    min = k;
+                    str3 = str1; // запоминаем самое короткое
+                }
+                if (k > max)
+                {
+                    max = k;
+                    str2 = str1; // запоминаем самое длинное
+                }
+            }
+
+            // сбрасываем счетчик и строку
+            k = 0;
+            str1 = "";
+        }
+    }
+
+    cout << "Самое короткое слово: " << str3 << '\n';
+    cout << "Самое длинное слово: " << str2 << '\n';
+
+    return 0;
+}
