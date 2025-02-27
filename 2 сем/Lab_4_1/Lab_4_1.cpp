3f80a65c7bb6d7da174795140fbc242b2b31f324
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-02-27 10:45:55 +0500

    Add files via upload

---
+++ 2 сем/Lab_4_1/Lab_4_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+float sum( int n, float a[])
+{
+	float summ{};
+	for (int i = 0; i < n; i++)
+	{
+		summ += a[i];
+	}
+	return summ;
+}
+
+void main()
+{
+	float x[] = { 30.5, 45.6, 78.9, 23.5, 12.7, 98.4, 64.9, 32.54 };
+	float s = sum(8, x);
+	cout << s << endl;
+}
+
+
