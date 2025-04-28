04a3eaec07f52c28c4eb5ce5914d3fa24fc32ca5
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-04-28 12:06:49 +0500

    Add files via upload

---
+++ 2 сем/Class/Lab_6/Lab6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Vector.h"
+
+using namespace std;
+
+int main()
+{
+	Vector a(5);
+	Vector b(5);
+	cin >> a;
+	cin>> b;
+	Vector sum = a + b;
+	cout << "\nSum: " << sum << endl;
+
+	cout << "First element: " << *a.first() << endl;
+
+	Iterator i = a.first();
+	i=i+1;
+	cout << "Second element: " << *i << endl;
+	for (i = a.first(); i != a.last();++i)cout << *i <<" ";
+	cout << endl;
+	return 0;
+}
