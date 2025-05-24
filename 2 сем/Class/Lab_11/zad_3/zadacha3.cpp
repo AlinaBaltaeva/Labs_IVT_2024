75df9016ec166862b2c0e24bb202fc115664769a
AlinaBaltaeva <baltaeva06@inbox.ru>
2025-05-25 00:49:17 +0500

    Add files via upload

---
+++ 2 сем/Class/Lab_11/zad_3/zadacha3.cpp
@@ -0,0 +1,43 @@
+﻿#include "Time.h"
+#include "Vector.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "Введите 5 элементов Time (минуты и секунды):" << endl;
+        Vector<Time> vec(5);
+
+        cout << "Исходный вектор:" << endl;
+        vec.Print();
+
+        vec.AddMax();
+        cout << "После добавления максимального элемента в начало:" << endl;
+        vec.Print();
+
+        Time avg = vec.Srednee();
+        cout << "Среднее арифметическое: " << avg << endl;
+
+        int pos;
+        cout << "Введите позицию для добавления среднего: ";
+        cin >> pos;
+        vec.Add(avg, pos);
+        cout << "После добавления среднего:" << endl;
+        vec.Print();
+
+        int max_pos = vec.Max();
+        vec.Del(max_pos);
+        cout << "\nПосле удаления максимального элемента:" << endl;
+        vec.Print();
+        vec.Delenie();
+        cout << "После деления на минимальный элемент:" << endl;
+        vec.Print();
+    }
+    catch (const char* msg) {
+        cerr << "Ошибка: " << msg << endl;
+    }
+    catch (...) {
+        cerr << "Неизвестная ошибка!" << endl;
+    }
+    return 0;
+}
